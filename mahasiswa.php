671fb4bcdfd81d4b94fa604664c4ae2f742ba82a
Naufal Faiz <naufalfaizhidayat135@gmail.com>
2024-07-08 13:36:48 +0700

    Tambah file mahasiswa.html

---
+++ mahasiswa.php
@@ -0,0 +1,24 @@
+<?php
+$conn = mysqli_connect(
+    $hostname = "localhost",
+    $username = "root",
+    $password = "",
+    $database = "db_mahasiswa"
+)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cobain GIT</title>
+</head>
+<body>
+    <h3>Data Mahasiswa</h3>
+    <ul>
+        <li>Naufal Faiz</li>
+    </ul>
+</body>
+</html>
